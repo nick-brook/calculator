a66c36b30a095502bb50b8b3047ca8e99fcab033
nick-brook <66472330+nick-brook@users.noreply.github.com>
2020-10-22 11:20:03 +0100

    operator checks

--- calc.js
+++ calc.js
@@ -1,98 +1,153 @@
 var formula = "";
 var answer = 0;
+// set regex to look for operators
+const regex = new RegExp(/\+|\-|\*|\//);
 
 function updateFormula(digit) {
 
-
-// set regex to look for operators
-    const regex = new RegExp(/\+|\-|\*|\//);
-    
     //  find last operator in string
     var chk = regex.test(formula);
     
 // if formula doesn't contain an operator
-
     if (!chk){
-        formula = handleZeroDec(digit,formula);
-    }
-// formula does have an operator
+        formula = handleDigit(digit,formula);
+        }
     else{
-
-// get the index of the last operator
-        var subForm = "";
+        // get the index of the last operator
         var i = getnum();
 
-        // if operator is last char 
-        if (i === formula.length - 1){
-            formula = handleZeroDec(digit,formula);
-        }
-
-        else {
-            subForm = formula.slice(i+1);
-            
-       // new formula is slice of formula incl index and concat sub formula
-            formula = formula.slice(0,i+1) + handleZeroDec(digit,subForm);
+        // check for invalid operator
+         if (multiOp(formula,digit)){
+            formula = formula.slice(0,i+1) + handleDigit(digit,formula.slice(i+1));
             }
-
-        
-    }
+        }   
     
     updateDisp(answer,formula)
 }
 
+
+function multiOp(formula,digit){
+
+// if digit is an operator check its valid
+    if ( regex.test(digit)){
+        
+        var chkchar = formula.slice(length-1)
+
+        // check if last char in formula is operator
+        if (regex.test(chkchar))
+            {
+                // check if valid operator
+                switch(chkchar) {
+                    case "+":
+                        // + already entered allow -
+                        if (digit === "-"){
+                            return true
+                            }
+                        else {
+                            return false
+                        }
+                      break;
+
+                      case "-":
+                        // + already entered allow -
+                        if (digit === "+"){
+                            return true
+                            }
+                        else {
+                            return false
+                        }
+                      break;
+
+                      case "*":
+                        // + already entered allow -
+                        if (digit === "+" || digit === "-"){
+                            return true
+                            }
+                        else {
+                            return false
+                        }
+                      break;
+
+                      case "/":
+                        // + already entered allow -
+                        if (digit === "+" || digit === "-"){
+                            return true
+                            }
+                        else {
+                            return false
+                        }
+                      break;
+
+                    default:
+                            return false
+                        break;
+                    }
+
+            }
+        else {
+            return true
+        }
+
+        }
+        // 
+    else{
+        return true
+        }
+}
+
+
 // function to return the index of the last operator
 function getnum(){
-    const indexRegex = new RegExp(/\+|\-|\*|\/|\./);
-    var j = formula.length -1;
+    
+    var j = formula.length;
 
-    for (j; j >= 0; j--){
-        // get last charcater in string
-        var chkchar = formula.substring(j,j + 1)
+    for (j; j > 0; j--){
+        // get last charcater in string then work backwards
+        var chkchar = formula.slice(j-1,j)
         // check if character is operator or decimal
-            if (indexRegex.test(chkchar)){
-                return j
+            if (regex.test(chkchar)){
+                return j-1
             }
     }
 
     return 0
 }
 
-function handleZeroDec(digit,chkStr) {
+// function handles input of new digit
+function handleDigit(digit,chkStr) {
 
-// if number starts with zero 
-    if (chkStr === "0" ){   
-    // and a zero is entered return same string
-        if (digit === "0"){
-            return chkStr
-        }
-// if zero already entered return "0." otherwise return digit
-        else {
-            if (digit === "."){
+// switch statement for digit
+    switch(digit) {
+        case "0":
+            // double zeros - don't append
+            if (chkStr === "0"){
+              return chkStr
+                }
+            else {
+                return chkStr + digit
+            }
+          break;
+        case ".":
+          // if no decimal already entered - append
+            if (chkStr.indexOf(".") === -1 ){
                 return chkStr + digit
                 }
-                else {
-                    return digit
+            else {
+                return chkStr
                 }
-            }
-        }
+            break;
+        default:
 
-// check if decimal not in string then add
-    if (chkStr.indexOf(".") === -1 ){
-            return chkStr + digit;
-         }
-
-    else {
-// check not an extra decimal
-        if (digit !== "."){
-            return chkStr + digit;
-            }
-        else {
-            return chkStr
-            }
+            // if string = "0" return digit only
+            if (chkStr === "0"){
+                return digit
+                }
+            else {
+                return chkStr + digit
+                }
+            break;
         }
 }
-
-
 
 
 function backSpace(){
@@ -112,14 +167,42 @@ function initialize() {
 function updateDisp(ans,wrk) {
 
     document.getElementById("idans").value = ans;
-    document.getElementById("iddisp").value = wrk;
+    document.getElementById("iddisp").innerHTML = wrk;
 }
 
 // calculate answer 
 function calculate() {
+  
+    answer = parseFloat(eval(formula));
+    
 
+// set answer to exp if v large or small 
+    if(answer >= 1000000000000 || answer <= -1000000000000 || (answer <= 0.00001 && answer >= -0.00001 && answer !== 0)){
+        answer = expo(answer,4);
+       }
+   else {
+        // max 8 decimal places
+         answer = answer.toFixed(8);
+          // set to max 10 decimals (convert answer back to a number datatype)
+         answer = Number(answer).toPrecision(10);
+         
+
+     // format large +ve and negative 
+     if (answer > 999 || answer < -999){
+        answer = new Intl.NumberFormat().format(answer)
+
+     }
+        // remove rounding anomalies
+        answer = (answer *1000) / 1000;
+        
    
-    answer = eval(formula);
-    updateDisp(answer,formula)
+
+        }
+
+    updateDisp(answer,formula);
 
 }
+
+function expo(x, f) {
+    return Number.parseFloat(x).toExponential(f);
+  }
